002e5d952c37ecd02bf726bf1fadfd87fbe16f09
JPoz <jpoz@jpoz.net>
2010-09-28 10:03:14 -0700

    adding first part of front end

--- public/application.js
+++ public/application.js
@@ -1,1 +1,55 @@
-var Dashy = {};
+var Dashy = {
+  init: function(list_selector) {
+    this.get(0);
+    this.list = $(list_selector);
+  },
+  get: function(revision) {
+    console.log('START POLL');
+    var self = this;
+    $.getJSON('/event/'+revision, function(e, textStatus){
+      console.log(textStatus)
+      if (textStatus == 'success') {
+        var array = e.list;
+        var timeout = 0
+        $.each(array, function( i, d ){
+          data = JSON.parse(d);
+          console.log(data);
+          self.list.prepend(self.build(data));
+        })
+
+        self.get(e.revision);
+      }
+    });
+  },
+  build: function(event) {
+    var title = $('<h3/>', {
+      html: event.title
+    });
+    var subtitle = $('<div/>', {
+      html: event.subtitle
+    });
+    var actor     = $('<strong/>', {
+      html: event.actor
+    })
+    var link_1     = $('<a/>', {
+      href: event.link_1_url,
+      target: '_blank',
+      html: event.link_1_title
+    })
+    
+    var container = $('<div/>', {
+      html: ""
+    });
+    
+    container
+      .append(actor).append(link_1)
+      .append(title)
+      .append(subtitle);
+    
+    return container;
+  }
+};
+
+$(function() {
+  Dashy.init('#event_list');
+})
